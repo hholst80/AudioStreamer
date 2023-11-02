e7d6550b2230080dbb1547145de67a9f13a008e9
Henrik Holst <6200749+hholst80@users.noreply.github.com>
2023-11-02 22:22:50 +0000

    Remove https, opener

--- server/server.js
+++ server/server.js
@@ -2,9 +2,8 @@
  * Created by noamc on 8/31/14.
  */
 var binaryServer = require('binaryjs').BinaryServer,
-    https = require('https'),
+    http = require('http'),
     wav = require('wav'),
-    opener = require('opener'),
     fs = require('fs'),
     connect = require('connect'),
     serveStatic = require('serve-static'),
@@ -16,19 +15,12 @@ var uaParser = new UAParser();
 if (!fs.existsSync("recordings"))
     fs.mkdirSync("recordings");
 
-var options = {
-    key: fs.readFileSync('ssl/server.key'),
-    cert: fs.readFileSync('ssl/server.crt'),
-};
-
 var app = connect();
 
 app.use(serveStatic('public'));
 
-var server = https.createServer(options, app);
+var server = http.createServer(app);
 server.listen(9191);
-
-opener("https://localhost:9191");
 
 var server = binaryServer({ server: server });
 
